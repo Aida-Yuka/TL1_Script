2804801347163fe33aec97725358dbe6695a1ac5
Aida_Yuka <K024G0052@g.neec.ac.jp>
2026-06-09 15:14:49 +0900

    Update level_editor.py
    01_04　変更

--- level_editor.py
+++ level_editor.py
@@ -14,14 +14,6 @@ bl_info = {
     "category":"Object"
 }
 
-#メニュー項目描画
-def draw_menu_manual(self,context):
-    #self : 呼び出し元のクラスインスタンス。c++でいうthisポインタ
-    #context : カーソルを合わせた時のポップアップのカスタマイズなどに使用
-   
-    #トップバーの「エディターメニュー」に項目(オペレータ)を追加
-    self.layout.operator("wm.url_open_preset",text="Manual",icon='HELP')
-
 #トップバーの拡張メニュー
 class TOPBAR_MT_my_menu(bpy.types.Menu):
     #Blenderがクラスを識別する為の固有の文字列
@@ -41,7 +33,7 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
     def submenu(self,context):
         
         #ID指定でサブメニューを追加
-            self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 
 #Blenderに登録するクラスリスト
 classes = (
@@ -60,12 +52,12 @@ def register():
 
 #Add-On無効化時コールバック
 def unregister():
+    #メニューから項目を削除
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+    
     #Blenderからクラスを削除
     for cls in classes:
         bpy.utils.unregister_class(cls)
-    
-    #メニューから項目を削除
-    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが無効化されました。")
     
 #テスト実行用コード
