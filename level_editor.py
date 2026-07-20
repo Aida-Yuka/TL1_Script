98c3d60eb1fe0d1ea4861985b23ca60fcaf3d713
Aida_Yuka <K024G0052@g.neec.ac.jp>
2026-07-20 12:04:58 +0900

    01_10

--- level_editor.py
+++ level_editor.py
@@ -92,6 +92,15 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
         #カスタムプロバティ'file_name'
         if "file_name" in object:
             self.write_and_print(file,indent + "N %s" % object["file_name"])
+        #カスタムプロバティ'collision'
+        if "collider" in object:
+            self.write_and_print(file,indent + "C %s" % object["collider"])
+            temp_str = indent + "CC %f %f %f"
+            temp_str %= (object["collider_center"][0],object["collider_center"][1],object["collider_center"][2])
+            self.write_and_print(file,temp_str)
+            temp_str = indent + "CS %f %f %f"
+            temp_str %= (object["collider_size"][0],object["collider_size"][1],object["collider_size"][2])
+            self.write_and_print(file, temp_str)
         self.write_and_print(file, indent + 'END')
         self.write_and_print(file, '')
 
@@ -331,7 +340,6 @@ class DrawCollider:
         shader.uniform_float("color",color)
         #描画
         batch.draw(shader)
-
 
 #Add-On有効化時コールバック
 def register():
